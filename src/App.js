6dfa065f9a80c11dc71881033a8a814d1753cbcc
James Bean <jkbea4@gmail.com>
2021-03-29 19:21:00 -0400

    "Commit App Build"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+
+
 
 function App() {
   return (
@@ -8,14 +11,21 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+       //  <Cofee uri="http://localhost:3001/JavaShare" />
+       // intended link for Cofee static app server
+       >
+               
+        
           Learn React
+          
+     
         </a>
       </header>
     </div>
