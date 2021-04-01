f0ba14751fafbf28f222b8fe60c67ea1568c2fd9
Luke Carr <lukecarr4547@gmail.com>
2021-03-31 19:56:50 -0500

    added inputs and navbar frame to home

--- src/loginPage/login.js
+++ src/loginPage/login.js
@@ -7,6 +7,19 @@ function login() {
 
     return (
         <div>
+            <Form>
+                <Form.Group controlId="formGroupEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
             
         </div>
     )
