a5d29ac7e88d19da7ce4960ba9cd4444068364c2
James Bean <jkbea4@gmail.com>
2021-04-02 18:07:25 -0400

    babel

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+require("@babel/register");
 import reportWebVitals from './reportWebVitals';
 
 
