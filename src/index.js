368cfe0da81ad8217061977d417a9f971d3ce02a
James Bean <55850449+jkbea303@users.noreply.github.com>
2021-04-03 09:12:08 -0400

    Update index.js
    Dev dependency of require Babel was removed from index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-require("@babel/register");
 import reportWebVitals from './reportWebVitals';
 
 
