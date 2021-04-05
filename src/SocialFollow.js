ad3cb0502a66644b0ec149b40dd938a88710f63f
James Bean <jkbea4@gmail.com>
2021-04-04 22:41:47 -0400

    commit

---
+++ src/SocialFollow.js
@@ -0,0 +1,20 @@
+import React from "react";
+import App from "./App";
+
+export default function SocialFollow() {
+this.props.mail 
+  return(
+ <Route path="app" component={App}>
+
+<div class="icon"></div><div class="social-links"> <ul class="right"> <li class="socialIcon print" onclick="window.print()"> <div> <span class="icon"></span> </div> </li> </ul> <ul class="left"> <li class="socialIcon email" data-share-id="email"> <a href="mailto:?subject=How%20to%20Add%20Social%20Media%20Buttons%20to%20a%20Facebook%20Business%20Page%20%7C%20&amp;body=I%20found%20this%20article%20on%20%20and%20thought%20you%20would%20enjoy%20it%21%0d%0dhttps%3A//smallbusiness.chron.com/add-social-media-buttons-facebook-business-28705.html" data-event="social share" data-info="Email" aria-label="Share via Email"> <span class="icon"></span> </a> </li> <li class="socialIcon facebook" data-share-id="facebook" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=https://smallbusiness.chron.com/add-social-media-buttons-facebook-business-28705.html', 'socialshare', 'width=550,height=450')" data-event="social share" data-info="Facebook" aria-label="Share on Facebook"> <div> <span class="icon"></span> </div> </li> <li class="socialIcon twitter" data-share-id="twitter"> <a href="#socialshare" onclick="window.open('http://twitter.com/share?url=https://smallbusiness.chron.com/add-social-media-buttons-facebook-business-28705.html&amp;text=How%20to%20Add%20Social%20Media%20Buttons%20to%20a%20Facebook%20Business%20Page', 'socialshare', 'width=550,height=450')" data-event="social share" data-info="Twitter" aria-label="Share on Twitter"> <span class="icon"></span> </a> </li> <li class="socialIcon pinterest" data-share-id="pinterest"> <div> <span class="icon"></span> </div> </li> <li class="socialIcon reddit" data-share-id="reddit"> <div> <span class="icon"></span> </div> </li> <li class="socialIcon flipboard" data-share-id="flipboard"> <div> <span class="icon"></span> </div> </li> </ul> </div>
+
+<ul class="right"> <li class="socialIcon print" onclick="window.print()"> <div> <span class="icon"></span> </div> </li> </ul>
+<li class="socialIcon print" onclick="window.print()"> <div> <span class="icon"></span> </div> </li>
+<ul class="right"> <li class="socialIcon print" onclick="window.print()"> <div> <span class="icon"></span> </div> </li> </ul>
+<ul class="left"> <li class="socialIcon email" data-share-id="email">
+ <a href="" data-event="social share" data-info="Email" aria-label="Share via Email"> <span class="icon"></span> </a> </li> <li class="socialIcon facebook" data-share-id="facebook" onclick="window.open('https://purple.com', 'socialshare', 'width=550,height=450')" data-event="social share" data-info="Facebook" aria-label="Share on Facebook"> <div> <span class="icon"></span> </div> </li> <li class="socialIcon twitter" data-share-id="twitter"> <a href="#socialshare" onclick="window.open('http://purple.com/', 'socialshare', 'width=550,height=450')" data-event="social share" data-info="Twitter" aria-label="Share on Twitter"> <span class="icon"></span> </a> </li> <li class="socialIcon pinterest" data-share-id="pinterest"> <div> <span class="icon"></span> </div> </li> <li class="socialIcon reddit" data-share-id="reddit"> <div> <span class="icon"></span> </div> </li> <li class="socialIcon flipboard" data-share-id="flipboard"> <div> <span class="icon"></span> </div> </li> </ul>
+    <li class="socialIcon email" data-share-id="email"> <a href = "mailto:`codewinit{'@'}gmail.com`" data-event="social share" data-info="Email" aria-label="Share via Email"></a> </li> <span class="icon" ></span>
+  
+  </Route>
+  );
+}
