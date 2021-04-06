323ff6bb56de5248d4c97ca82b2d192c6327ea4a
Luke Carr <lukecarr4547@gmail.com>
2021-04-06 17:59:19 -0500

    working nav bar

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import css from './index.css';
 
 
 
 
+console.log("cottoncandy")
 ReactDOM.render(
   <React.StrictMode>
     <App />
