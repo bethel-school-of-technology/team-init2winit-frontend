b491405579e9dc0eb43eaa8461c00ae2fe9a9447
Austin Klein <austinklein34@gmail.com>
2021-04-09 12:15:00 -0600

    connected to backend and auth done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Homepage from './homePage/HomePage';
 import Login from './loginPage/Login';
 import Signup from './signupPage/Signup';
+import PrivateRoute from './routing/PrivateRoute';
 
 
 
@@ -11,7 +12,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={Homepage} />
+        <PrivateRoute exact path='/' component={Homepage} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Signup} />
       </Switch>
