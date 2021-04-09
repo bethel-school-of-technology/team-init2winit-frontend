040eb7c11f2a2edc0db6c857d97d55eb5f5b7fd8
Austin Klein <austinklein34@gmail.com>
2021-04-09 14:25:19 -0600

    fixing stuff

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Homepage from './homePage/HomePage';
-import Login from './loginPage/login';
-import Signup from './signupPage/signup';
-import SocialFollow from './SocialFollow';
-import Homepage from './homePage/HomePage';
 import Login from './loginPage/Login';
 import Signup from './signupPage/Signup';
 import PrivateRoute from './routing/PrivateRoute';
@@ -13,7 +9,6 @@ import PrivateRoute from './routing/PrivateRoute';
 
 
 function App() {
-
 
   return (
     <Router>
@@ -23,18 +18,9 @@ function App() {
         <Route exact path='/register' component={Signup} />
       </Switch>
     </Router>
-    
-  return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/register' component={Signup} />
-      </Switch>
-
   )
-}
 
+}
 export default App;
 
-     
+
