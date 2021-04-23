9885935d9f47f97c4d03ec0a1e57e262028e0df7
Austin Klein <austinklein34@gmail.com>
2021-04-23 13:35:46 -0600

    fix

--- src/googlemaps/googlemaps.js
+++ src/googlemaps/googlemaps.js
@@ -6,7 +6,7 @@ function Googlemaps() {
 
         <div>
             <NavComponent />
-            <iframe src="https://www.google.com/maps/d/embed?mid=1TWjK-qadigTRp5rr6LB1l4e-JoE&hl=en" width="640" height="480"></iframe>
+            <iframe className="map" src="https://www.google.com/maps/d/embed?mid=1TWjK-qadigTRp5rr6LB1l4e-JoE&hl=en" width="100%" height="1000"></iframe>
         </div>
     )
 }
