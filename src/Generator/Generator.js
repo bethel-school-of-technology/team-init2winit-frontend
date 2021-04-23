d8257d94d63b9ac79b1beda3b9ce32647e594c59
Luke Carr <lukecarr4547@gmail.com>
2021-04-23 15:25:54 -0500

    added gen data

--- src/Generator/Generator.js
+++ src/Generator/Generator.js
@@ -13,29 +13,62 @@ function Generator() {
     // this is mock data that will soon move to back end
     const data = [
         {
-            name: 'coffee 1',
-            desc: 'a cool description',
-            image: 'https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG'
+            name: 'Americano',
+            desc: 'A strong shot of espresso in hot water',
+            image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.starbucks.com%2Fmenu%2Fproduct%2F406%2Fhot%3Fparent%3D%252Fdrinks%252Fhot-coffees%252Famericanos&psig=AOvVaw2xVXTkHLoeU2HlX-EMJmYO&ust=1619294541967000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMC2hdyUlfACFQAAAAAdAAAAABAD',
+            strong: true
         },
         {
-            name: 'coffee 2',
-            desc: 'a cool description',
-            image: 'https://media3.s-nbcnews.com/j/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p_67dfb6820f7d3898b5486975903c2e51.fit-1240w.jpg'
+            name: 'Double shot vanilla latte',
+            desc: 'Two shots of espresso with a half serving of steamed milk',
+            image: 'https://globalassets.starbucks.com/assets/c8d2c8fa22ee4f4c864dee6dac5a1868.jpg?impolicy=1by1_wide_1242',
+            strong: true
         },
         {
-            name: 'coffee 3',
-            desc: 'a cool description',
-            image: 'http://www.southsoundtalk.com/wp-content/uploads/2019/11/Coffee-ander-Latte-e1573143205948.jpg'
+            name: 'Black dark roast columbian (pour-over)',
+            desc: 'Fine ground imported columbian beans slow dripped to perfection',
+            image: 'https://globalassets.starbucks.com/assets/0279f9c5fa5941d2a65dd183d7a0b386.jpg?impolicy=1by1_wide_1242',
+            strong: true
         },
         {
-            name: 'coffee 4',
-            desc: 'a cool description',
-            image: 'https://img1.mashed.com/img/gallery/coffee-mistakes-youre-probably-making-at-home/intro-1594766282.jpg'
+            name: 'Turkish cinnamon finisher',
+            desc: 'A traditional turkish dessert, made with spice and dark roast coffee',
+            image: 'https://www.alshaya.com/campaigns/Starbucks/SBX_CoffeeAuthority/images/winter-campaign/turkish%20coffee.jpg',
+            strong: true
         },
         {
-            name: 'coffee 5',
-            desc: 'a cool description',
-            image: 'https://cdn.cancercenter.com/-/media/ctca/images/others/blogs/2019/01-january/01-blog-coffee-l.jpg'
+            name: 'Nitrogen infused cold brew',
+            desc: 'Coffee slow brewed through cold process and nitrogen infused',
+            image: 'https://globalassets.starbucks.com/assets/ac2a4461dfe6481a9647ef3544d31c22.jpg',
+            strong: true
+        },
+        {
+            name: 'Affogato',
+            desc: 'A shot of espresso poured over a scoop of vanilla ice cream',
+            image: 'https://data.thefeedfeed.com/recommended/post_1323899.jpg',
+            strong: false
+        },
+        {
+            name: 'Salted Caramel latte',
+            desc: 'Salted Caramel in espresso with steamed milk',
+            image: 'https://globalassets.starbucks.com/assets/1039a0883ad548b69c5f93b9f76dbae2.jpg?impolicy=1by1_wide_1242',
+            strong: false
+        },
+        {
+            name: 'Flat white',
+            desc: 'Steamed milk with your choice of flavor',
+            image: 'https://globalassets.starbucks.com/assets/fedee22e49724cd09fbcc7ee2e567377.jpg?impolicy=1by1_wide_1242',
+            strong: false
+        }, {
+            name: 'Mocha Latte',
+            desc: 'Latte rich with chocolate goodness',
+            image: 'https://globalassets.starbucks.com/assets/f4ec500b34624242b15c2d29e53f3c48.jpg?impolicy=1by1_wide_1242',
+            strong: false
+        }, {
+            name: 'Cappuccino',
+            desc: 'Shot of espresso with 50% steamed milk',
+            image: 'https://globalassets.starbucks.com/assets/5c515339667943ce84dc56effdf5fc1b.jpg?impolicy=1by1_wide_1242',
+            strong: false
         },
     ];
 
