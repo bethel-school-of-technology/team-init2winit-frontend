e2d7415a003df87415233bc0fc54261a43c5d793
Austin Klein <70716038+austin-klein@users.noreply.github.com>
2021-04-25 19:42:10 -0600

    Revert "Insert src james1"

--- src/SocialFollow.js
+++ src/SocialFollow.js
@@ -1,36 +1,25 @@
 import React from "react";
 import reactDom from "react-dom";
-import HomePage from "./homePage/HomePage";
-
 
 
 export default function SocialFollow() {
   return(
-  <p path="SocialFollow" component={reactDom}>
+ <p path="SocialFollow" component={reactDom}>
 
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 
-          <div class="icon-bar">
-            <html class="icon" >
-                  <a href="/Users/jamesbean/team-init2winit-frontend/public/photos/f_logo_RGB-Black_58.png" class="facebook"><i class="fa fa-facebook"></i></a> 
-                  <a href="/Users/jamesbean/team-init2winit-frontend/public/photos/Twitter.png" class="twitter"><i class="fa fa-twitter"></i></a> 
-                  <a href="/Users/jamesbean/team-init2winit-frontend/public/photos/glyph-logo_May2016-310x310.png" class="instagram"><i class="fa fa-instagram"></i></a>
-                    <button redirect = "src/contact.html">ClickMe</button>
-            </html>
-          </div>
-  </p>
 
-    );
-  }
-  class HomePage extends React.Component {
-    render() {
-      return (
-        <div>
-        <h1>Home Page</h1>
-        <SocialFollow />
-        </div>
-      );
-    }
-  }
+<div class="icon-bar">
+<html class="icon" >
+  <a href="/Users/jamesbean/team-init2winit-frontend/public/photos/f_logo_RGB-Black_58.png" class="facebook"><i class="fa fa-facebook"></i></a> 
+  <a href="/Users/jamesbean/team-init2winit-frontend/public/photos/Twitter.png" class="twitter"><i class="fa fa-twitter"></i></a> 
+  <a href="/Users/jamesbean/team-init2winit-frontend/public/photos/glyph-logo_May2016-310x310.png" class="instagram"><i class="fa fa-instagram"></i></a>
+ <button redirect = "mailto:codewinit@gmail.com"/>
+  </html> 
+</div>
+
+
   
-  ReactDOM.render(< HomePage/>, document.getElementById('root'));
+  </p>
+  );
+}
